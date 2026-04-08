1bce455e5a452abbeccbb91c6c84f0fadd16d8cb
Josh Murray <jmurray2011@gmail.com>
2026-04-08 02:06:45 -0500

    Use debug.BuildInfo for version when installed via go install

--- cmd/cob/main.go
+++ cmd/cob/main.go
@@ -3,14 +3,25 @@ package main
 import (
 	"fmt"
 	"os"
+	"runtime/debug"
 
 	"github.com/jmurray2011/cob/internal/cli"
 )
 
 var version = "dev"
 
+func resolveVersion() string {
+	if version != "dev" {
+		return version
+	}
+	if info, ok := debug.ReadBuildInfo(); ok && info.Main.Version != "" && info.Main.Version != "(devel)" {
+		return info.Main.Version
+	}
+	return version
+}
+
 func main() {
-	root := cli.NewRootCmd(version)
+	root := cli.NewRootCmd(resolveVersion())
 	if err := root.Execute(); err != nil {
 		fmt.Fprintf(os.Stderr, "Error: %s\n", err)
 		os.Exit(1)
